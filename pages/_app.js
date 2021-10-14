6422684cf95fe449aa09f5afc7e4976b1a485334
fahiyanamira <fahiyanamira56@gmail.com>
2021-10-14 11:22:04 +0700

    convert sign up page

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "../styles/detail.css";
 import "../styles/checkout.css";
 import "../styles/complete-checkout.css";
 import "../styles/sign-in.css";
+import "../styles/sign-up.css";
 import Head from "next//head";
 
 function MyApp({ Component, pageProps }) {
