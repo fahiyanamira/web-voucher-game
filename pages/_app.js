9e351c8e996ba9b9c3f4f72c79688f19479ce9c0
fahiyanamira <fahiyanamira56@gmail.com>
2021-10-15 09:04:56 +0700

    convert member transactions page

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ import "../styles/sign-up-success.css";
 import "../styles/404-not-found.css";
 import "../styles/sidebar.css";
 import "../styles/overview.css";
+import "../styles/transactions.css";
 import Head from "next//head";
 
 function MyApp({ Component, pageProps }) {
