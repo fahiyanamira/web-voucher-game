19f7d1192a84a61b271f64e432547622f893443f
fahiyanamira <fahiyanamira56@gmail.com>
2021-10-20 19:59:38 +0700

    menambahkan hasil refactor member transaction ddetail

---
+++ components/organisms/TransactionDetailContent/Row.jsx
@@ -0,0 +1,8 @@
+export default function Row(props) {
+  const { label, value, className } = props;
+  return (
+    <p className="text-lg color-palette-1 mb-20">
+      {label} <span className={`purchase-details ${className}`}>{value}</span>
+    </p>
+  );
+}
