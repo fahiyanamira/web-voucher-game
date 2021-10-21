5f8275a4f7d7e1f3651d8b5659e4612b56dfbd63
fahiyanamira <fahiyanamira56@gmail.com>
2021-10-21 19:31:27 +0700

    membuat design system component input dengan storybook

---
+++ stories/components/atoms/Input/Input.stories.jsx
@@ -0,0 +1,15 @@
+// import { Meta } from "@storybook/react";
+import React from "react";
+import Input from "../../../../components/atoms/Input";
+
+export default {
+  title: "Components/Atoms/Input",
+  component: Input,
+};
+
+const Template = (args) => <Input {...args} />;
+//generate component kita ke storybooknya:
+export const Default = Template.bind({});
+Default.args = {
+  label: "Nama Lengkap",
+};
